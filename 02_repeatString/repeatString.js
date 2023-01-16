72f929aaeba95dde790022cc838d5890a9e06b2a
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-16 14:41:03 -0600

    course work completed and test passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
+
 const repeatString = function() {
-
-};
-
+var hey =""
+    for (let i = 0; i < 3; i++) {
+        /* Runs 3 times, with values of step 0 through 2.*/
+            hey += ('hey');
+      }
+      return hey;
+      
+    };
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arRay, ...varib) {
+    //declare values needed to be searched
+    var rem=varib;
+    var keeP="";
+    // filter multiple arguments from array
+    keeP = arRay.filter(item => !rem.includes(item));
+    //return new results to computer
+    return keeP;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
