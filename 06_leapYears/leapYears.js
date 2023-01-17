31303a5ba9b35f506d1399f3f74731df86373718
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-16 23:10:30 -0600

    leap year file updated with 4 statements to provide peoper LEAP YEAR results

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(entryYear) {
 
+let modulo = entryYear % 4;
+let centuryModulo = entryYear % 400;
+let centuryModuloByHun = entryYear % 100;
+
+if (centuryModuloByHun === 0 && centuryModulo === 0) {
+    return true;
+}else if(centuryModuloByHun === 0 ) {
+    return false;
+}else if ( modulo === 0 || centuryModulo === 0 ) {
+    return true;
+}else {
+return false;
+}
 };
 
 // Do not edit below this line
