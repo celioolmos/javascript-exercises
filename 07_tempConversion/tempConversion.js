80a0ed61b5eb3ffb7c2a6f7a7881b1995f28c9e0
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-16 23:44:33 -0600

    updated temp conversion files so both conversion tables operate as expected. all results will be showin to the 1st decimal point.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+temp = ((fTemp-32) *5)/9
+roundedTemp = temp.toFixed(1)
+return parseFloat(roundedTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+temp = (cTemp*1.8) + 32  
+roundedTemp = temp.toFixed(1)
+return parseFloat(roundedTemp);
 };
 
 // Do not edit below this line
