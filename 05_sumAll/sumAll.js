fa4b1902fc27b0812480619e3ea2fb2c84094c91
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-16 22:22:54 -0600

    sumall js file updated to return desired results.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
-
-};
+const sumAll = function(xNum, yNum) {
+    
+    let start = xNum;
+    let end = yNum;
+    let sum = 0;
+    const myArray = [];
+    
+    
+    if (xNum < 0 || yNum < 0) {
+        return "ERROR";
+    } else if ( isNaN(xNum) || isNaN(yNum) ) {
+        return "ERROR";
+    } else if ( typeof xNum === "string"  || typeof yNum === "string" ) {
+        return "ERROR";
+    } else if (start < end){
+        for (let i = start; i <= end; i++ ) {
+       myArray.push(i)
+        } myArray.forEach(item => {
+        sum += item
+      })
+      return sum; 
+    } else if (xNum > yNum) {
+           
+        for (let i = xNum; i >= yNum; --i) {
+           myArray.push(i)
+        } 
+        myArray.forEach(item => {
+            sum += item
+          })
+        return sum; 
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
