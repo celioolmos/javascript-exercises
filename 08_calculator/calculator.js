4e4698bc53b25fa37fde5a4de935c1d7607ebc35
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-17 10:22:13 -0600

    calculator js file updated to perform needed functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,58 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(suppliedArr) {
+  const myArray = suppliedArr
+  const initialValue = 0;
+
+const sumWithInitial = myArray.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+ return sumWithInitial;
 };
 
-const multiply = function() {
+const multiply = function(suppliedArr) {
+  const myArray = suppliedArr
+  const initialValue = 1;
 
+const sumWithInitial = myArray.reduce(
+  (accumulator, currentValue) => accumulator * currentValue,
+  initialValue
+);
+ return sumWithInitial;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	result = num1 ** num2;
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num1) {
+  if (num1 < 0) 
+  return -1;
+else if (num1 == 0 ||num1 == 1) 
+return 1;
+else {
+  let result = 1;
+  if (num1 == 0 || num1 == 1) {
+    return result;
+  } else {
+    for (var i = num1; i >= 1; i--) {
+      result = result * i;
+    }
+    return result;
+  }
+}
+
+}
 
 // Do not edit below this line
 module.exports = {
