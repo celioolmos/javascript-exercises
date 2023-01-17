03191329eab661d45172a62b7d01cdc9313f7e97
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-17 11:19:56 -0600

    palindrom exercise completed. Regen , lowercase , split , and join are all functions updated to accomplish needed tasks.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let reGen =  /[^A-Za-z0–9]/g ;
+let lowreGenString = string.toLowerCase().replace(reGen, '');
+let splitString = lowreGenString.split("");
+let reverseArray = splitString.reverse();
+let joinArray = reverseArray.join("");
 
+
+if (joinArray === lowreGenString) {
+    return true;
+}else {
+    return false;
+}
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// using https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/ to help with solution. reGen x was used.
