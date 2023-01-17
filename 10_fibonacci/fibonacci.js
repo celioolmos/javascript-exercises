7e857fcc5339dcf9d8be1cbdd7cc84575587396d
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-17 14:05:50 -0600

    code updated to relay correct fib sequence based on user input.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,36 @@
-const fibonacci = function() {
+const fibonacci = function(userEntry) {
+    var userNum 
+    var num1=0;
+    var num2=1;
+    var sum;
+    var i=0;
+    nextSeq = ""
 
-};
+    if (userEntry < 0 ) {
+        return "OOPS";
+    } else if (userEntry === 1 || userEntry === 2) {
+            nextSeq = 1;
+            return nextSeq;
+    } else if (typeof userEntry === "string)"){
+        userNum = parseFloat.userEntry
+        for (let i = 1; i < userNum; i++) {
+            sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return num2;
 
+   } else {
+        userNum = userEntry 
+        for (let i = 1; i < userNum; i++) {
+            sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return num2;
+        }
+    }
 // Do not edit below this line
 module.exports = fibonacci;
+
+
