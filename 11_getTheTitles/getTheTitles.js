ffce8b01a53148d6c47eaa243ad9d68ffac91f7b
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-17 19:53:12 -0600

    added array (callback function) for desired result.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
-};
-
+    const titles = array.map(book => book.title); 
+           return titles;
+     }
 // Do not edit below this line
 module.exports = getTheTitles;
