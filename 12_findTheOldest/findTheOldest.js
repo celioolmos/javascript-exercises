55aea21922f77283f30e4504182cef3302525803
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-19 00:15:08 -0600

    this test has been by far the most difficult. I had to receive help to solve solution. the reduce() function was needed and proper notation gave the biggest problems. thanks you to stackOverflow and ODIN forums for the help.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
-};
-
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldestAge < currentAge ? currentPerson : oldest
+      })
+    }
+    
+    const getAge = function(birth, death) {
+      if (!death) {
+        death = new Date().getFullYear();
+      }
+      return death - birth;
+    
+    }
+    console.log(getAge);
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+/*
+updating array with new values
+https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+
+
+
+finding the max value
+https://stackoverflow.com/questions/46088623/javascript-return-occurence-of-highest-score-in-array
+}
+
+
+const peopleAge = array.map ( people => { 
+    let ageProp = {
+        "Age": people.yearOfDeath - people.yearOfBirth,
+        "Name": people.name
+        };
+
+    return ageProp
+});
+
+
+const max = peopleAge.reduce(function(a, b){
+    if(a.Age > b.Age)
+        return a;
+    return b;
+})
+
+//const ageResult = max.map (a => a.Name);
+console.log(max);
+
+*/
