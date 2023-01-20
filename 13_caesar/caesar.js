00072aef2e7fa2dcd275eb774b4b1b9e30332493
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-20 12:27:44 -0600

    individual function created to split string into array , add incrimental value to each array item, build back string from array for final result. having issues with final result not equal to "answer key

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,72 @@
-const caesar = function() {
+const caesar = function(str, shift) {
 
-};
+    var varUpdatedString = ""
 
+// convert string to array of unicode
+const strToUni = function(string) {
+        var endArray = []
+        for (let i = 0; i < str.length; i++) {
+        var theUnicode = string.charCodeAt(i) ;
+        endArray.push(theUnicode)  
+    }
+    return endArray
+}
+ var unicodeArray = strToUni(str); 
+    
+
+       
+
+
+
+// incriment each letter by designated "shift" requirement
+shiftedUnicodeArray = unicodeArray.map(function(val){return val + shift})
+
+
+//return encrypted message back
+const regroupString = function(array){
+      varUpdatedString = ""
+    for (let i = 0; i <= shiftedUnicodeArray.length; i++) {
+stringPart = String.fromCharCode(shiftedUnicodeArray[i])
+ varUpdatedString = varUpdatedString + stringPart;}
+ return varUpdatedString;
+}
+
+
+regroupString(shiftedUnicodeArray)
+return varUpdatedString
+}
 // Do not edit below this line
 module.exports = caesar;
+
+
+
+
+// convert each letter to unicode
+
+
+
+
+
+/*
+let message = str;
+let positionMove = shift
+
+for (let i = 0; i <= str.length; i++) {
+    var heUnicode = str.charCodeAt(i) ;
+    var newMessage = "";
+    var changeMessage ;
+    
+        if (heUnicode > 64 && heUnicode < 91){
+            var changeMessage = String.fromCharCode(heUnicode + shift);
+            newMessage =+ newMessage.concat(changeMessage);
+        } else if (heUnicode > 96 && heUnicode < 123){
+            var changeMessage = String.fromCharCode(heUnicode + shift);
+            newMessage =+ newMessage.concat(changeMessage);
+        } else (
+            newMessage =+ newMessage.concat(changeMessage));
+        
+
+        } 
+        
+return newMessage;
+  }*/
