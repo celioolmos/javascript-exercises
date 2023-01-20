ede8d771c79a8d93a4c4fd5636ec49dc1cbb0e6e
Bay3ic <47784991+celioolmos@users.noreply.github.com>
2023-01-20 12:31:16 -0600

    deleted trash code

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -37,36 +37,3 @@ return varUpdatedString
 }
 // Do not edit below this line
 module.exports = caesar;
-
-
-
-
-// convert each letter to unicode
-
-
-
-
-
-/*
-let message = str;
-let positionMove = shift
-
-for (let i = 0; i <= str.length; i++) {
-    var heUnicode = str.charCodeAt(i) ;
-    var newMessage = "";
-    var changeMessage ;
-    
-        if (heUnicode > 64 && heUnicode < 91){
-            var changeMessage = String.fromCharCode(heUnicode + shift);
-            newMessage =+ newMessage.concat(changeMessage);
-        } else if (heUnicode > 96 && heUnicode < 123){
-            var changeMessage = String.fromCharCode(heUnicode + shift);
-            newMessage =+ newMessage.concat(changeMessage);
-        } else (
-            newMessage =+ newMessage.concat(changeMessage));
-        
-
-        } 
-        
-return newMessage;
-  }*/
